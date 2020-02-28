12f8e0e9c3e3d30abc0fe8327748732aab33c714
himadyuti98 <honey.bhanja@gmail.com>
2020-02-28 18:24:51 +0530

    offensive languages detected

--- open.py
+++ open.py
@@ -8,6 +8,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import sys
 import re
+from profanity_check import predict, predict_prob
+
 
 
 driver = webdriver.Firefox(executable_path=r'./Gecko/geckodriver')
@@ -18,14 +20,26 @@ wait = WebDriverWait(driver, 600)
 def strip_tags(html):
     return re.sub('<[^<]+?>', '', html)
 
-def send_message(target, string):
+'''def send_message(target, string):
     x_arg = '//span[contains(@title,' + target.lower() + ')]'
     group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
     group_title.click()
     message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
     message.send_keys(string)
     sendbutton = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/button')[0]
-    sendbutton.click()
+    sendbutton.click()'''
+
+def send_message(target, string):
+    x_arg = '//span[contains(@title,' + target + ')]'
+    print(x_arg)
+    group_title = wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))
+    print(group_title)
+    group_title.click()
+    #message = driver.find_element(By.XPATH , '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]') #find_elements_by_xpath('//*[@id="main"]/footer/div[0]/div[1]/div[0]/div[1]')[0]
+    inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
+    input_box = wait.until(EC.presence_of_element_located((By.XPATH, inp_xpath))) 
+    print(input_box)
+    input_box.send_keys(string + Keys.ENTER)
 
 def mainMessage(sleep=0):
     time.sleep(sleep)
@@ -52,11 +66,16 @@ def mainMessage(sleep=0):
 
             for message in messages:
                 mess = strip_tags(message.get_attribute('innerHTML'))
-                group_name = "'" + classify(mess)[0] + "'"
+                mlist = []
+                mlist.append(mess)
+                is_offensive = predict(mlist)[0]
+                if is_offensive:
+                    send_message("'Offensive'", mess)
+                '''group_name = "'" + classify(mess)[0] + "'"
                 print(mess)
                 print(group_name)
 
-                send_message(group_name, mess)
+                send_message(group_name, mess)'''
         except :
             pass
 #            alert1 = driver.SwitchTo().Alert()
